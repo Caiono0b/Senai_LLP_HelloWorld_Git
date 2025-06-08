0cec5f95eb9b9f0c2bf2acc61cb5735ae74efdc5
Caiono0b <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>
2025-06-08 16:30:31 -0300

    hello interativo

---
+++ HelloWorldInterativo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorldInterativo {
+    public static void main(String[] args) { 
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nome = leia.nextLine();
+
+        System.out.println("Hello World " + nome);
+
+        leia.close();
+    }
+}
+
