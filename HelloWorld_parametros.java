e90cbc9b08c1dc62d0cbc3a8852d37a9f7b45ae4
Caiono0b <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>
2025-06-08 15:52:51 -0300

    ola mundo com parametros agora vai

---
+++ HelloWorld_parametros.java
@@ -0,0 +1,14 @@
+public class HelloWorld_parametros {
+
+public void saudacao(String ola) {
+    System.out.println(ola);
+    
+    }
+    public static void main(String[] args){
+    
+        HelloWorld_parametros h =new HelloWorld_parametros();
+
+        h.saudacao("Hello World");
+    }
+    
+}
