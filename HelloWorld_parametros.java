ff33459acfa81625d4774d7f799a9b6b993f8309
Caiono0b <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>
2025-06-08 16:35:33 -0300

    joia

--- HelloWorld_parametros.java
+++ HelloWorld_parametros.java
@@ -1,14 +1,14 @@
 public class HelloWorld_parametros {
 
 public void saudacao(String ola) {
-    System.out.println(ola);
+    System.out.println("Hello World"+ola);
     
     }
     public static void main(String[] args){
     
         HelloWorld_parametros h =new HelloWorld_parametros();
 
-        h.saudacao("Hello World");
+        h.saudacao(" for you");
     }
     
 }
